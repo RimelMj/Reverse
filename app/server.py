f219968baa5af1cc801c0eb8b85bc9f87b51c4c1
Rimel Morjane <37373286+RimelMj@users.noreply.github.com>
2020-04-29 02:51:21 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/9lcuo2krf3vpe6j/Deploy.ipynb?dl=0'
-export_file_name = 'Deploy.ipynb'
+export_file_url = 'https://www.dropbox.com/s/ellhgssk3018oyi/model.pkl?dl=0'
+export_file_name = 'model.pkl'
 
 classes = ['M.33cl.Boga',
  'M.33cl.Coca',
