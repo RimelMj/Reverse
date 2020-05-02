471ae755913db55e60d21cb7c2568cc977b2b4af
Rimel Morjane <37373286+RimelMj@users.noreply.github.com>
2020-05-02 04:58:49 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1QQkSIllNBrv_wsn-PiZxQGUeKVsZkLn'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1F6W1KaEVfwDWISAXP2isSICfQ2ythYW'
 export_file_name = 'export.pkl'
 
 classes = ['M.33cl.Boga',
